03e44a729665d87e54da57d55d79580e75237e8c
Jameson Miller <jamill@microsoft.com>
2022-08-02 15:09:45 +0000

    fixup postcreatecommand

--- .devcontainer/clone-pytorch.sh
+++ .devcontainer/clone-pytorch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# Change into /workspaces directory
+cd ..
+
 # Check if the PyTorch repository exists
 if [ ! -d "pytorch" ]
 then
-    echo "Directory does not exist"
-    # git clone https://github.com/pytorch/pytorch
+    echo "Cloning pytorch repository"
+    git clone https://github.com/pytorch/pytorch
 fi
